c6a2a25f3279281c6545ecc90a6ebc639e71588b
Daniyar Jakupov <ddr199305@gmail.com>
2018-07-02 21:32:51 +0200

    Test custom-redux with diff actions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* =============================== REDUX IMPLEMENTATION ================================ */
 function createStore(reducer) {
   /* The store should have 4 parts:  */
   // 1. Create the state
@@ -32,24 +33,70 @@ function createStore(reducer) {
     dispatch
   };
 }
-
-/** Helper reducer and action creator */
-const addAction = () => ({
-  type: 'ADD',
-  todo: {
-    id: 0,
-    name: 'Read',
-    complete: false
-  }
-});
-
-// Reducer
+/** ============================================================================================= */
+/** ====================== Create reducers and action creators =================================== */
+// Reducers
 function todoReducer(state = [], action) {
   switch (action.type) {
-    case 'ADD':
+    case "ADD":
       return [...state, action.todo];
-      break;
+    case "REMOVE":
+      return state.filter(todo => todo.id !== action.id);
+    case "TOGGLE":
+      return state.map(
+        todo =>
+          todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+      );
     default:
       return state;
   }
 }
+
+function goalsReducer(state = [], action) {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return [...state, action.goal];
+    case "REMOVE_GOAL":
+      return state.filter(goal => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function rootReducer(state = {}, action) {
+  return {
+    todos: todoReducer(state.todos, action),
+    goals: goalsReducer(state.goals, action)
+  };
+}
+
+const addAction = payload => ({
+  type: "ADD",
+  todo: payload
+});
+const removeAction = id => ({
+  type: "REMOVE",
+  id
+});
+const toggleAction = id => ({
+  type: "TOGGLE",
+  id
+});
+const addGoalAction = payload => ({
+  type: "ADD_GOAL",
+  goal: payload
+});
+
+/** ========================================================= */
+// Run this
+const store = createStore(rootReducer);
+store.subscribe(() => {
+  console.log("The new state is: ", store.getState());
+});
+store.dispatch(addAction({ id: 0, name: "Implement Redux", completed: false }));
+store.dispatch(addAction({ id: 1, name: "Learn TS", completed: false }));
+store.dispatch(addAction({ id: 2, name: "Learn Reason", completed: false }));
+store.dispatch(toggleAction(0));
+store.dispatch(
+  addGoalAction({ id: 0, name: "Land a cool gig", completed: false })
+);
